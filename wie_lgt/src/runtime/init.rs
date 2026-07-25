2f54b6a35746804f544b495df2d4476c4d144582
jjongjjongs <jjongjjongs@users.noreply.github.com>
2026-07-25 23:52:20 +0900

    Add Lm runtime slot 0xf8 diagnostic stub

--- wie_lgt/src/runtime/init.rs
+++ wie_lgt/src/runtime/init.rs
@@ -355,6 +355,7 @@ pub async fn load_native(core: &mut ArmCore, system: &mut System, jvm: &Jvm, dat
     let lm_stub_110 = core.make_svc_stub(SVC_CATEGORY_INIT, JAVA_DIAG_SVC_BASE + 0x110)?;
 
     write_generic(core, 0x01500a50, lm_stub_f0)?;
+    write_generic(core, 0x01500a58, lm_stub_f8)?;
     write_generic(core, 0x01500a5c, lm_stub_fc)?;
     write_generic(core, 0x01500a64, lm_stub_104)?;
     write_generic(core, 0x01500a68, lm_stub_108)?;
