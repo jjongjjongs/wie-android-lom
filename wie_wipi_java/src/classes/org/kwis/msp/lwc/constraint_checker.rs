de8df8e2b160c61f42a98a1c456b053705650e2f
jjongjjongs <you@example.com>
2026-08-12 11:45:03 +0900

    Restore WIPI ConstraintChecker data validation

--- wie_wipi_java/src/classes/org/kwis/msp/lwc/constraint_checker.rs
+++ wie_wipi_java/src/classes/org/kwis/msp/lwc/constraint_checker.rs
@@ -1,7 +1,7 @@
 use alloc::vec;
 
 use java_class_proto::{JavaFieldProto, JavaMethodProto};
-use jvm::{ClassInstanceRef, Jvm, Result as JvmResult};
+use jvm::{Array, ClassInstanceRef, JavaChar, Jvm, Result as JvmResult};
 
 use wie_jvm_support::{WieJavaClassProto, WieJvmContext};
 
@@ -21,6 +21,12 @@ impl ConstraintChecker {
                     "<init>",
                     "(Lorg/kwis/msp/lwc/TextComponent;)V",
                     Self::init,
+                    Default::default(),
+                ),
+                JavaMethodProto::new(
+                    "checkData",
+                    "([CII)Z",
+                    Self::check_data,
                     Default::default(),
                 ),
             ],
@@ -58,5 +64,71 @@ impl ConstraintChecker {
             .await?;
 
         Ok(())
+    }
+
+    async fn check_data(
+        jvm: &Jvm,
+        _: &mut WieJvmContext,
+        this: ClassInstanceRef<Self>,
+        data: ClassInstanceRef<Array<JavaChar>>,
+        offset: i32,
+        length: i32,
+    ) -> JvmResult<bool> {
+        let constraint: i32 = jvm
+            .get_field(&this, "__wieConstraint", "I")
+            .await?;
+
+        if constraint != 1 && constraint != 2 && constraint != 5 {
+            return Ok(true);
+        }
+
+        if length <= 0 {
+            return Ok(true);
+        }
+
+        if data.is_null() {
+            return Err(
+                jvm.exception("java/lang/NullPointerException", "")
+                    .await,
+            );
+        }
+
+        if offset < 0 {
+            return Err(
+                jvm.exception("java/lang/ArrayIndexOutOfBoundsException", "")
+                    .await,
+            );
+        }
+
+        let chars = jvm
+            .load_array::<JavaChar>(&data, offset as usize, length as usize)
+            .await?;
+
+        for ch in chars {
+            let ch = ch as u16;
+
+            let valid = match constraint {
+                1 => {
+                    (ch >= b'0' as u16 && ch <= b'9' as u16)
+                        || ch == b' ' as u16
+                        || ch == b'-' as u16
+                }
+                2 => ch >= b'0' as u16 && ch <= b'9' as u16,
+                5 => {
+                    (ch >= b'0' as u16 && ch <= b'9' as u16)
+                        || ch == b' ' as u16
+                        || ch == b'-' as u16
+                        || ch == b'+' as u16
+                        || ch == b'#' as u16
+                }
+                _ => true,
+            };
+
+            if !valid {
+                return Ok(false);
+            }
+        }
+
+        Ok(true)
     }
 }
